111a5e7683e478db810deecf34e78503313ef196
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-09-04 18:01:12 +0300

    First task of the homework

---
+++ src/PrintYourHometown.java
@@ -0,0 +1,8 @@
+/**
+ * Created by stenly on 9/4/14.
+ */
+public class PrintYourHometown {
+    public static void main(String[] args) {
+        System.out.println("My hometown is Gorno Nanadolnishte!");
+    }
+}
