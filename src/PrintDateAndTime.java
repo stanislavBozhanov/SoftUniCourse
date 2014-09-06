cd525fd307a15a206f2b9b33eecfe4c4fdc87bcd
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-09-06 13:32:50 +0300

    Changes to project

--- src/PrintDateAndTime.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class PrintDateAndTime {
-    public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println(currentDate);
-    }
-}
